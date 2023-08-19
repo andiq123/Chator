c67e68a0402775fb8e87dc889c6f84ff5116ef76
Andiq123 <andy.u789@gmail.com>
2023-08-19 19:55:54 +0300

    added 2 users

--- api/Data/DataContext.cs
+++ api/Data/DataContext.cs
@@ -5,4 +5,31 @@ public class DataContext : IdentityDbContext<User>
     public DataContext(DbContextOptions<DataContext> options) : base(options) { }
     public DbSet<Message>? Messages { get; set; }
 
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        var hasher = new PasswordHasher<User>();
+
+        var user1 = new User
+        {
+            Id = "1",
+            UserName = "user1",
+            NormalizedUserName = "USER1",
+            Description = "I am the user 1",
+            PasswordHash = hasher.HashPassword(null, "Password")
+        };
+
+        var user2 = new User
+        {
+            Id = "2", 
+            UserName = "user2",
+            NormalizedUserName = "USER2",
+            Description = "I am the user 2",
+            PasswordHash = hasher.HashPassword(null, "Password") 
+        };
+
+        builder.Entity<User>().HasData(user1, user2);
+    }
+
 }
